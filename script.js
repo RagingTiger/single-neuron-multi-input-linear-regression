e3d5fcea8e4cf6783bc616883687f3bca463a015
Glitch (hello-webpage) <none>
2021-12-23 23:53:16 +0000

    💾🏩 Checkpoint ./script.js:9162534/917

--- script.js
+++ script.js
@@ -20,3 +20,29 @@ if (status) {
   status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
 }
 
+// An array of input data
+const houseFeatures = [
+  // Size, Bedrooms
+  [720, 1],
+  [863, 1],
+  [674, 1],
+  [600, 1],
+  [760, 1],
+  [982, 1],
+  [1513, 2],
+  [1073, 2],
+  [1185, 2],
+  [1222, 2],
+  [1060, 2],
+  [1575, 2],
+  [1440, 3],
+  [1787, 3],
+  [1551, 3],
+  [1653, 3],
+  [1575, 3],
+  [2522, 3]
+];
+
+
+// Estimated dollar cost of house for each piece of data above (1000x square footage).
+const housePrice = [800000, 850000, 900000, 950000, 980000, 1000000, 1050000, 1075000, 1100000, 1150000, 1200000,  1250000 , 1300000, 1400000, 1500000, 1600000, 1700000, 1800000, 1900000, 2000000];
