771428ba5bcc230d52273acf221fe1ecd6d729b4
Glitch (hello-webpage) <none>
2021-12-24 00:04:30 +0000

    🖇⏲ Checkpoint ./script.js:9162534/1644

--- script.js
+++ script.js
@@ -44,5 +44,42 @@ const houseFeatures = [
 ];
 
 
-// Estimated dollar cost of house for each piece of data above (1000x square footage).
-const housePrice = [800000, 850000, 900000, 950000, 980000, 1000000, 1050000, 1075000, 1100000, 1150000, 1200000,  1250000 , 1300000, 1400000, 1500000, 1600000, 1700000, 1800000, 1900000, 2000000];
+// Current listed house prices in dollars given their features above.
+const housePrices = [971000, 875000, 620000, 590000, 710000, 849000, 1995000, 1199000, 1380000, 1398888, 1650000, 1498000, 1782000, 1987888, 1688000, 1850000, 1498000, 5900000];
+
+
+
+// Now actually create and define model architecture.
+const model = tf.sequential();
+
+// We will use one dense layer with 1 neuron and an input of 
+// a single value.
+model.add(tf.layers.dense({inputShape: [1], units: 1}));
+
+// Choose a learning rate that is suitable for the data we are using.
+const LEARNING_RATE = 0.0001;
+
+train();
+
+async function train() {
+  // Compile the model with the defined learning rate and specify
+  // our loss function to use.
+  model.compile({
+    optimizer: tf.train.sgd(LEARNING_RATE),
+    loss: 'meanSquaredError'
+  });
+
+  // Finally do the training itself over 500 iterations of the data.
+  // As we have so little training data we use batch size of 1.
+  // We also set for the data to be shuffled each time we try 
+  // and learn from it.
+  let results = await model.fit(trainTensors.data, trainTensors.answer, {epochs: 200, batchSize: 1, shuffle: true});
+  
+  // Once trained we can evaluate the model.
+  evaluate();
+}
+
+async function evaluate(stuff) {
+  // Predict answer for a single piece of data.
+  model.predict(tf.tensor2d([[768, 2]])).print();
+}
